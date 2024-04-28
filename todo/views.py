3974636e144aa0cffcd6c023ca9b7dda0a2ddef6
javad <j.2528840@gmail.com>
2024-04-28 17:06:21 +0330

    respons

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse, StreamingHttpResponse, JsonResponse
 import json
 from .models import Todo
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 
-def get_users(request):
+@api_view(["GET"])
+def get_users(request: Request):
     json_data = list(Todo.objects.all().values())
-    # json_data = json.dumps({"data": json_data})
 
-    return JsonResponse({"data": json_data})
+    # json_data = json.dumps({"data": json_data})
+    # return JsonResponse({"data": json_data})
+
+    return Response({"data": json_data}, status.HTTP_200_OK)
