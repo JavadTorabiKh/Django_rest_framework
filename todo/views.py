f8c9004e93f2d4b12cb65e354b73e1a0ba7f6109
javad <j.2528840@gmail.com>
2024-04-29 12:32:58 +0330

    post

--- todo/views.py
+++ todo/views.py
@@ -9,11 +9,16 @@ from rest_framework.decorators import api_view
 from .serializers import TodoSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def get_users(request: Request):
-    json_data = list(Todo.objects.all().values())
-    todo_serializer = TodoSerializer(json_data, many=True)
-    # json_data = json.dumps({"data": json_data})
-    # return JsonResponse({"data": json_data})
-    print(todo_serializer)
-    return Response(todo_serializer.data, status.HTTP_200_OK)
+    if request.method == "GET":
+        json_data = list(Todo.objects.all().values())
+        todo_serializer = TodoSerializer(json_data, many=True)
+
+        return Response(todo_serializer.data, status.HTTP_200_OK)
+    elif request.method == "POST":
+        todo_serializer = TodoSerializer(data=request.data)
+        if todo_serializer.is_valid():
+            todo_serializer.save()
+            return Response({"status": "success", "data": todo_serializer.data}, status.HTTP_201_CREATED)
+        return Response({"status": "erorr"}, status.HTTP_400_BAD_REQUEST)
